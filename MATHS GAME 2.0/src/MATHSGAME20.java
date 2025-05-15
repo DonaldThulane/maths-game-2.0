a3ba4ed82150b90320aa53a4a4b2643fe42aab1a
DonaldThulane <thulanemakhubela11@gmail.com>
2025-05-15 19:06:07 +0200

    Add files via upload

---
+++ MATHS GAME 2.0/src/MATHSGAME20.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+///import maths.game.pkg2.pkg0.MATHSGAME20;
+
+/**
+ *
+ * @author EDUV5956036
+ */
+public class MATHSGAME20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new NewJFrame1().setVisible(true);
+    }
+    
+}
